d08c3b1a66175bb5dbeb51e4d07fb038d8fd1a74
matt wilkie <mapew@gmail.com>
2024-11-25 09:05:13 -0700

    attempt to use persistent storage, hardcoded for now

--- start-docker.sh
+++ start-docker.sh
@@ -3,5 +3,8 @@
 [ ! -z "${USER_UID}" ] && usermod -u ${USER_UID} node || echo "No USER_UID specified, leaving 1000"
 [ ! -z "${USER_GID}" ] && groupmod -og ${USER_GID} node || echo "No USER_GID specified, leaving 1000"
 
+# for fly.io. Volume 'data' must exist and be mounted to /data
+export TRILIUM_DATA_DIR=/data
+
 chown -R node:node /home/node
 exec su -c "node ./src/main" node
