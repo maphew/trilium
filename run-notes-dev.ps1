08537f09e77cb8988958164c449290e60a44a78a
Matt Wilkie <matt.wilkie@yukon.ca>
2024-12-10 12:56:39 -0700

    WIP: almost have working behind corporate firewall
    ...that unwaps all packets in transit, which all sane SSL interpret as
    conducting man in the middle attacks.

--- run-notes-dev.ps1
+++ run-notes-dev.ps1
@@ -1,8 +1,14 @@
 #!/usr/bin/env pwsh
 
+# Assumes Dockerfile.notes-dev has been built
+# run that image and insert these local files into it
+
 # Stop and remove existing container if it exists
 podman stop notes-dev 2>$null
 podman rm notes-dev 2>$null
+
+# Build development image
+podman build -t notes-dev -f Dockerfile.notes-dev .
 
 # Run the container with mounted volumes
 podman run -d `
