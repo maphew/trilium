0b04131907ed9259a23e6d4a2fd4f2d4a8851bc8
matt wilkie <maphew@gmail.com>
2024-12-16 21:37:07 -0700

    quick test script

---
+++ wsl-test.cmd
@@ -0,0 +1,11 @@
+@echo ---
+wsl --shutdown 
+@echo ---
+type %userprofile%\.wslconfig 
+@echo.
+@echo ---
+wsl nslookup google.com 
+@echo ---
+wsl wget google.com && if exist index.html del index.html
+@echo ---
+wsl ping -c2 google.com
